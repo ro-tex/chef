dffcdc0e80c88d45520e885bd2c7041824931f12
Ivaylo Novakov <inovakov@gmail.com>
2016-06-21 17:07:30 +0300

    Add a template for our service.

--- silverbubble/cookbooks/ro-tex_ruby/recipes/default.rb
+++ silverbubble/cookbooks/ro-tex_ruby/recipes/default.rb
@@ -9,3 +9,14 @@
 
 include_recipe 'rvm::system_install'
 
+# https://docs.chef.io/resource_file.html
+
+# Use the cookbook_file resource to copy a file from a cookbook’s /files directory. 
+# Use the template resource to create a file based on a template in a cookbook’s /templates 
+# directory. And use the remote_file resource to transfer a file to a node from a remote location.
+template '/etc/init.d/optimo_ui' do
+  source 'optimo_ui.erb'
+  mode '0755'
+  owner 'root'
+  group 'root'
+end
