3b0664bb686774893dd0c7678ab04617e0902669
Ivaylo Novakov <inovakov@gmail.com>
2016-08-05 15:40:43 +0300

    Adjusting the spark cookbook to use spark 2.0.0.

--- silverbubble/cookbooks/apache_spark/attributes/default.rb
+++ silverbubble/cookbooks/apache_spark/attributes/default.rb
@@ -16,14 +16,14 @@
 default['apache_spark']['install_mode'] = 'tarball'
 
 default['apache_spark']['download_url'] =
-  'http://d3kbcqa49mib13.cloudfront.net/spark-1.5.2-bin-hadoop2.6.tgz'
+  'http://d3kbcqa49mib13.cloudfront.net/spark-2.0.0-bin-hadoop2.7.tgz'
 # The SHA256 checksum of the downloaded tarball.
 default['apache_spark']['checksum'] =
-  '409c4b34f196acc5080b893b0579cda000c192fc4cc9336009395b2a559b676e'
+  '3d46e990c06a362efc23683cf0ec15e1943c28e023e5b5d4e867c78591c937ad'
 
 # These options are used with the "package" installation mode.
 default['apache_spark']['pkg_name'] = 'spark'
-default['apache_spark']['pkg_version'] = '1.5.2'
+default['apache_spark']['pkg_version'] = '2.0.0'
 
 default['apache_spark']['install_dir'] = '/usr/share/spark'
 default['apache_spark']['install_base_dir'] = '/opt/spark'
