d181f5318c150461ff7dad28d81fffc503faabf8
Ivaylo Novakov <inovakov@gmail.com>
2016-08-09 15:46:51 +0300

    Removing those cookbooks again...

--- silverbubble/cookbooks/apache_spark/recipes/default.rb
+++
@@ -1,5 +0,0 @@
-#
-# Cookbook Name:: apache_spark
-# Recipe:: default
-#
-# Copyright (c) 2016 The Authors, All Rights Reserved.
--- silverbubble/cookbooks/learn_chef_apache2/attributes/defualt.rb
+++
@@ -1,2 +0,0 @@
-default['ro-tex_ruby']['ruby_version']	= '2.3.1'
-default['ro-tex_ruby']['rails_version']	= '2.3.1'
