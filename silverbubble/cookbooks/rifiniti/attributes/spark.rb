aeec15ebe9ddc387f20b20131339693ba3e01bd8
Ivaylo Novakov <inovakov@gmail.com>
2016-08-09 16:48:41 +0300

    Clean up.

--- silverbubble/cookbooks/rifiniti/attributes/spark.rb
+++ silverbubble/cookbooks/rifiniti/attributes/spark.rb
@@ -1,8 +1,3 @@
-# set up some java attributes
-default['java']['jdk_version'] = '7' # there is no openjdk 8 support and we don't like oracle
-default['java']['accept_license_agreement'] = true
-default['java']['set_etc_environment'] = true
-
 # set up some apache spark attributes
 default['apache_spark']['download_url'] =
   'http://d3kbcqa49mib13.cloudfront.net/spark-2.0.0-bin-hadoop2.7.tgz'
