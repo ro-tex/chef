9459d1f6df005b56ddb33b63dbed6c42124473c4
Ivaylo Novakov <inovakov@gmail.com>
2016-10-21 14:58:30 +0300

    Even if we define the role attributes as 'default' they end up in node['override']. Maybe that's because we add them to override_attributes? :)

--- silverbubble/roles/ruby.rb
+++ silverbubble/roles/ruby.rb
@@ -2,7 +2,7 @@ name 'ruby'
 description 'This is a role for all servers that need Ruby'
 run_list 'recipe[ro-tex_ruby::default]'
 override_attributes(
-  override: {
+  default: {
     'ro-tex_ruby': {
       ruby_version: '2.3.1',
       rails_version: '4.2.7.1'
