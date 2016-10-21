39eb879b1746c5043baba8dfbb2e11c390043439
Ivaylo Novakov <inovakov@gmail.com>
2016-10-21 14:55:39 +0300

    Add an override.

--- silverbubble/roles/ruby.rb
+++ silverbubble/roles/ruby.rb
@@ -2,11 +2,9 @@ name 'ruby'
 description 'This is a role for all servers that need Ruby'
 run_list 'recipe[ro-tex_ruby::default]'
 override_attributes(
-  default: {
-    ro-tex_ruby: {
-      ruby_version: '2.3.1'
-    },
-    ro-tex_ruby: {
+  override: {
+    'ro-tex_ruby': {
+      ruby_version: '2.3.1',
       rails_version: '4.2.7.1'
     }
   }
